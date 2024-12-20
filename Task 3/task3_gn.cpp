c7af85a18e67aefe507b15dde3124c906839c974
Rana Ayman <ranaayman2468@gmail.com>
2024-12-21 01:02:41 +0200

    modify task 3

--- Task 3/task3_gn.cpp
+++ Task 3/task3_gn.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main(){
-    string s;
+    string plaintext;
     int x ;
     int a,b;//keys
     cout<<"enter the string you want to cipher:\n";
-    getline(cin, s);
+    getline(cin, plaintext);
     cout<<"enter the keys of the cipher:\n";
     cin>>a>>b;
 
-     if (gcd(a, 94) != 1) {
-        cout << "Key 'a' must be coprime with 94.\n";
+     if (gcd(a, 95) != 1) {
+        cout << "Key 'a' must be coprime with 95.\n";
         return 1; // Exit with an error
     }
 
-    int size = s.length();//size of the strig to cipher
-    for(int i=0 ; i<size ; i++){
+    string encrypted = plaintext;
+    for(auto & character : encrypted ){
 
-        x = s[i] - 33;//offset the characters to start from zero
-        s[i] = ((a * x + b) % 94) + 33;//apply affine cipher 94 is length of the sequence of symbols +33 to return characters to their real values
-        cout<<s[i];
-
+        x = character - 32;//offset the characters to start from zero
+        character = ((a * x + b) % 95) + 32;//apply affine cipher 95 is length of the sequence of symbols +32 to return characters to their real values
     }
+
+    cout<<"the encrypted string is:"<<encrypted;
 }
